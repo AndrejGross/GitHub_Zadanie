1620ff0bd2008aeef48de465cb8fa24721bf04c6
Andrej Gross <andrej99991111@gmail.com>
2018-12-02 02:59:08 +0100

    Funkcia n

--- main.c
+++ main.c
@@ -13,6 +13,55 @@ struct inzeraty
     struct inzeraty *dalsi;
 };
 
+struct inzeraty* n(FILE **file,int *pocet_zaznamov,struct inzeraty *prvy,struct inzeraty *act)
+{
+    int pocetZaznamov=0;
+    char str[201];
+
+    if((*file=fopen("auta.txt","r"))==NULL)
+        return 0;
+
+    if (*file)
+    {
+        while(fscanf(*file,"%s",str)!=EOF)
+            if(str[0]=='$')
+                pocetZaznamov++;
+    }
+    *pocet_zaznamov=pocetZaznamov;
+
+    rewind(*file);
+
+    if((prvy=malloc(sizeof(struct inzeraty)))==NULL)
+        return 0;
+
+    act=prvy;
+    while(!feof(*file))
+    {
+        fscanf(*file,"%*c");
+        fscanf(*file,"%*c");
+        fscanf(*file,"%[^\n]",act->kategoria);
+        fscanf(*file,"%*c");
+        fscanf(*file,"%[^\n]",act->znacka);
+        fscanf(*file,"%*c");
+        fscanf(*file,"%[^\n]",act->predajca);
+        fscanf(*file,"%*c");
+        fscanf(*file,"%d",&act->cena);
+        fscanf(*file,"%*c");
+        fscanf(*file,"%d",&act->rok_vyroby);
+        fscanf(*file,"%*c");
+        fscanf(*file,"%[^\n]",act->stav_vozidla);
+        fscanf(*file,"%*c");
+
+        if((act->dalsi=malloc(sizeof(struct inzeraty)))==NULL)
+            return 0;
+
+        act=act->dalsi;
+    }
+    printf("Nacitalo sa %d zaznamov\n",pocetZaznamov);
+
+    return prvy;
+}
+
 int main()
 {
     char c;
